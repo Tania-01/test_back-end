9f1f6167ff411bfc873b29c91014834425e1b1a4
tetiana <tchirska@gmail.com>
2025-03-18 19:13:50 +0200

    first commit

--- src/midleware/authMidleware.ts
+++ src/midleware/authMidleware.ts
@@ -6,14 +6,14 @@ interface JwtPayload {
 }
 const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
     try {
-        // Отримуємо токен з заголовків запиту
+
         const token = req.headers['authorization']?.split(' ')[1];
 
         if (!token) {
             return res.status(401).json({ message: 'No token provided' });
         }
 
-        // Перевірка токену
+
         const decoded = jwt.verify(token, process.env.JWT_SECRET || 'your_jwt_secret') as JwtPayload;
 
 
--- src/service/authService.ts
+++ src/service/authService.ts
@@ -26,28 +26,24 @@ class AuthService {
     }
 
     public async login({ email, password }: { email: string, password: string }) {
-        // Знайти користувача по email
         const existingUser = await userRepository.findByEmail(email);
 
-        // Якщо користувача не знайдено, викинути помилку
         if (!existingUser) {
             throw new Error('Invalid email or password');
         }
 
-        // Порівняти введений пароль з тим, що є в базі даних
+
+
         const isMatch = await bcrypt.compare(password, existingUser.password);
 
-        // Якщо паролі не співпадають, викидаємо помилку
+
         if (!isMatch) {
             throw new Error('Invalid email or password');
         }
-
-        // Якщо все вірно, генеруємо JWT токен
         const token = jwt.sign({ userId: existingUser.user_id }, process.env.JWT_SECRET || 'your_jwt_secret', {
             expiresIn: '1h',
         });
 
-        // Повертаємо користувача та токен
         return { existingUser, token };
     }
 
